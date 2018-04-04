0509ffad06dd02a0dbaa3399176c7cdf84b2783d
Sasha <Sasha@Sashas-Mac-mini.local>
2018-04-04 12:31:57 +0300

    checked 4 elements on a page

--- src/test/java/CreditElements.java
+++ src/test/java/CreditElements.java
@@ -1,10 +1,16 @@
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
+
 public class CreditElements extends BeforeAfter {
+
     @Test
     public void creditElements() throws InterruptedException {
-       driver.findElement(By.xpath("//span[@class='m-top-i-text']")).click();
+        driver.findElement(By.xpath("//html//div[@class='clearfix body-header-row-top']//li[2]")).click();
+        Assert.assertTrue(driver.findElements(By.xpath("//div[@class='credit-rules-list-i']")).size()==4);
+        //Assert.assertEquals(driver.findElements(By.xpath("//div[@class='credit-rules-list-i']")).size(),4);
 
     }
 }
